9573d3fef683aef2d42f441e873408d97e64fccd
srojgam0907 <srojgam0907@g.educaand.es>
2026-01-29 14:38:38 +0100

    estructura de funciones javaScript version 1

---
+++ script.js
@@ -0,0 +1,30 @@
+const formulario= document.querySelector("#form");
+const mensaje= document.querySelector("#textoError");
+
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault(); //evita que la pagina se recargue
+
+    const texto= document.querySelector("#numero");
+    const numeros= texto.split(",");
+
+    //comprobarAnalisis
+    //analisisNumero
+    //Mostrar resultados
+
+}); 
+
+function comprobarAnalisis() {
+
+
+
+};
+
+function analisisNumeros() {
+    //Cantidad total de numeros
+    //Cuantos son positivos
+    //Cuantos son negativos
+    //Cuantos son 0
+    //El numero mayor
+    //El numero menor
+
+};
